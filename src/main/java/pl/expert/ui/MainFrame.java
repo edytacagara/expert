8f3d932e09c0bff5a54693bd214cc79c745d6f8a
krzysztof.zagraba <krzysztof.kzagraba@gmail.com>
2016-04-14 23:23:46 +0200

    dodany menuBar, dodany fileChooser

--- src/main/java/pl/expert/ui/MainFrame.java
+++ src/main/java/pl/expert/ui/MainFrame.java
@@ -5,37 +5,70 @@
  */
 package pl.expert.ui;
 
+import java.io.File;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class MainFrame extends Application 
-{
+public class MainFrame extends Application {
 
+    private final static Logger LOGGER = Logger.getLogger(MainFrame.class.getName()); 
+    
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Test button");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Test button clicked");
-            }
-        });
+    public void start(Stage primaryStage) {       
+        BorderPane root = new BorderPane();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 600, 480);
         
         primaryStage.setTitle("Expert main frame");
         primaryStage.setScene(scene);
+        
+        createMenuBar(root);
+        
         primaryStage.show();
+    }
+    
+    private void createMenuBar(BorderPane root) {
+        MenuBar menuBar = new MenuBar();
+        
+        Menu menuFile = new Menu("File");
+        MenuItem add = new MenuItem("Load knowledge base");
+        add.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                loadFile();
+            }
+        });  
+        
+        MenuItem exit = new MenuItem("Exit"); 
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        }); 
+ 
+        menuFile.getItems().addAll(add, exit);
+ 
+        Menu menuEdit = new Menu("Edit");
+        Menu menuView = new Menu("View");
+ 
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+        root.setTop(menuBar);
+    }
+    
+    private void loadFile() {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        LOGGER.info(selectedFile.getName() + " loaded");
     }
     
 }
