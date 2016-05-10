5f9444a00916d4da7002878c7292bf8477956d79
Mariusz Batyra <mbatyra@wp.eu>
2016-05-10 21:08:24 +0200

    refaktor + constraint resolver

--- src/main/java/pl/expert/core/engine/Engine.java
+++ src/main/java/pl/expert/core/engine/Engine.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import pl.expert.core.database.knowledge.Constraint;
 import pl.expert.core.database.knowledge.Knowledge;
 import pl.expert.core.database.knowledge.Model;
 import pl.expert.core.database.knowledge.Rule;
@@ -26,6 +27,7 @@ public class Engine {
     private static final Logger LOG = Logger.getLogger(Engine.class.getName());
     private final Map<String, Boolean> resolved = new LinkedHashMap<>();
 
+    // TODO dopoki wszyskie reguly nie zostana rozwiazane
     public void doMagic(final Knowledge knowledge) {
         for (Rule rule : knowledge.getRules()) {
             for (String condition : rule.getConditions()) {
@@ -33,6 +35,12 @@ public class Engine {
                 Boolean conditionResolved = resolved.get(condition);
                 if (conditionResolved == null) {
                     resolveModel(knowledge.getModels(), condition);
+                    conditionResolved = resolved.get(condition);
+                    if (conditionResolved == null) {
+                        conditionResolved = readBoolean(condition);
+                        resolved.put(condition, conditionResolved);
+                    }
+                    resolveConstraint(knowledge.getConstraints(), condition);
                 } else if (!conditionResolved) {
                     LOG.log(Level.INFO, "Condition: {0} is false", condition);
                     break;
@@ -49,7 +57,7 @@ public class Engine {
         for (Model model : models) {
             if (condition.equalsIgnoreCase(model.getResult())) {
                 // TODO zapamietac argumenty aby nie pytac np 2 razy o temp
-                BigDecimal userValue = read(model.getArgument());
+                BigDecimal userValue = readBigDecimal(model.getArgument());
                 boolean resolve = ModelResolver.resolve(userValue, model);
                 model.setResolved(resolve);
                 resolved.put(condition, resolve);
@@ -59,7 +67,18 @@ public class Engine {
         }
     }
 
-    private BigDecimal read(String question) {
+    private void resolveConstraint(final List<Constraint> constraints, final String condition) {
+        boolean conditionResolved = resolved.get(condition);
+        for(Constraint i : constraints) {
+            if(i.getConstraint().get(0).equalsIgnoreCase(condition)) {
+                resolved.put(i.getConstraint().get(1), !conditionResolved);
+            } else if(i.getConstraint().get(1).equalsIgnoreCase(condition)) {
+                resolved.put(i.getConstraint().get(0), !conditionResolved);
+            }
+        }
+    }
+
+    private BigDecimal readBigDecimal(String question) {
         System.out.println(question);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
@@ -69,7 +88,18 @@ public class Engine {
             LOG.log(Level.SEVERE, "Error while input reading! {0}", e.getMessage());
             return null;
         }
+    }
 
+    private boolean readBoolean(String question) {
+        System.out.println(question);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String str = bufferedReader.readLine();
+            return "y".equalsIgnoreCase(str);
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Error while input reading! {0}", e.getMessage());
+            return false;
+        }
     }
 
 }
