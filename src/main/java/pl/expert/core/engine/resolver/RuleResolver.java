c24a7c7f9ec511b24cac16ec532d05ad2fccde65
Mariusz Batyra <mbatyra@wp.eu>
2016-05-31 12:54:18 +0200

    refaktor

--- src/main/java/pl/expert/core/engine/resolver/RuleResolver.java
+++ src/main/java/pl/expert/core/engine/resolver/RuleResolver.java
@@ -20,30 +20,47 @@ public class RuleResolver {
 
     public static void resolve(final Knowledge knowledge) {
         Resolved.clearChanged();
-        for (Rule rule : knowledge.getRules()) {
-            boolean ruleResult = true;
-            for (String condition : rule.getConditions()) {
-                LOG.log(Level.INFO, "Processing condition: {0}", condition);
-                Boolean conditionResolved = Resolved.get(condition);
-                if (conditionResolved == null) {
-                    ModelResolver.resolve(knowledge.getModels(), condition);
-                    conditionResolved = Resolved.get(condition);
-                    if (conditionResolved == null) {
-                        conditionResolved = UserAnswerReader.getAnswear(condition, UserAnswerReader.BOOLEAN_QUESTION, Boolean.class);
-                        Resolved.put(condition, conditionResolved);
-                    }
-                    ConstraintResolver.resolveConstraint(knowledge.getConstraints(), condition);
-                } else if (!conditionResolved) {
-                    LOG.log(Level.INFO, "Condition: {0} is false", condition);
-                    ruleResult = false;
-                    break;
-                }
-            }
-            rule.setResolved(ruleResult);
+        knowledge.getRules().stream().forEach((rule) -> {
+            rule.setResolved(resolveRule(knowledge, rule));
 //            Resolved.put(rule.getResult(), ruleResult);
 //            Resolved.putWithDuplicates(rule.getResult(), ruleResult);
-            
-        }
+        });
     }
 
+    private static boolean resolveRule(final Knowledge knowledge, Rule rule) {
+        for (String condition : rule.getConditions()) {
+            LOG.log(Level.INFO, "Processing condition: {0}", condition);
+            Boolean conditionResolved = Resolved.get(condition);
+            if (conditionResolved == null) {
+                Rule conditionAsRule = conditionAsRule(knowledge, condition);
+                if(conditionAsRule != null) {
+                    resolveRule(knowledge, conditionAsRule);
+                }
+                ModelResolver.resolve(knowledge.getModels(), condition);
+                conditionResolved = Resolved.get(condition);
+                if (conditionResolved == null) {
+                    conditionResolved = UserAnswerReader.getAnswear(condition, UserAnswerReader.BOOLEAN_QUESTION, Boolean.class);
+                    Resolved.put(condition, conditionResolved);
+                }
+                if(!conditionResolved) {
+                    return conditionResolved;
+                }
+                ConstraintResolver.resolveConstraint(knowledge.getConstraints(), condition);
+            } else if (!conditionResolved) {
+                LOG.log(Level.INFO, "Condition: {0} is false", condition);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Rule conditionAsRule(final Knowledge knowledge, String condition) {
+        for(Rule rule : knowledge.getRules()) {
+            if(rule.getResult().equalsIgnoreCase(condition)) {
+                return rule;
+            }
+        }
+        return null;
+    }
+    
 }
